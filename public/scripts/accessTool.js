93a3cff0dbed52a84e1bddf6c447ee8b425f04e5
Shay Liberman <shayaliberman@gmail.com>
2024-02-27 00:58:10 +0200

    fixed code, need to improve path

---
+++ public/scripts/accessTool.js
@@ -0,0 +1,8 @@
+// accessTool.js
+document.write('<script src="https://code.jquery.com/jquery-1.12.4.js"><\/script>');
+document.write('<script src="https://www.negishim.com/accessibility/accessibility_pro.js"><\/script>');
+
+// Configuration for accessibility_pro.js
+accessibility_rtl = true;
+pixel_from_side = 20;
+pixel_from_start = 20;
--- views/accessTool.js
+++ views/accessTool.js
@@ -1,8 +1,20 @@
 // accessTool.js
-document.write('<script src="https://code.jquery.com/jquery-1.12.4.js"><\/script>');
-document.write('<script src="https://www.negishim.com/accessibility/accessibility_pro.js"><\/script>');
+
+// Include jQuery
+var jqueryScript = document.createElement('script');
+jqueryScript.src = 'https://code.jquery.com/jquery-1.12.4.js';
+jqueryScript.defer = true;
+document.head.appendChild(jqueryScript);
+
+// Include accessibility_pro.js
+var accessibilityScript = document.createElement('script');
+accessibilityScript.src = 'https://www.negishim.com/accessibility/accessibility_pro.js';
+accessibilityScript.defer = true;
+document.head.appendChild(accessibilityScript);
 
 // Configuration for accessibility_pro.js
-accessibility_rtl = true;
-pixel_from_side = 20;
-pixel_from_start = 20;
+var accessibility_rtl = true;
+var pixel_from_side = 20;
+var pixel_from_start = 20;
+
+// Your additional code for accessTool.js goes here
