89a3f1d0bb18513d4f3baa246c8c07899e3ac288
Shay Liberman <shayaliberman@gmail.com>
2024-03-14 13:23:15 +0200

    accessTool will now appear on left side

--- public/scripts/accessTool.js
+++ public/scripts/accessTool.js
@@ -1,6 +1,3 @@
-// accessTool.js
-
-// Include jQuery
 var jqueryScript = document.createElement('script');
 jqueryScript.src = 'https://code.jquery.com/jquery-1.12.4.js';
 jqueryScript.defer = true;
@@ -10,11 +7,17 @@ document.head.appendChild(jqueryScript);
 var accessibilityScript = document.createElement('script');
 accessibilityScript.src = 'https://www.negishim.com/accessibility/accessibility_pro.js';
 accessibilityScript.defer = true;
+
+// Define a function to configure accessibility settings after accessibility_pro.js is loaded
+function configureAccessibilitySettings() {
+    // Configuration for accessibility_pro.js
+    accessibility_rtl = false;
+    pixel_from_side = 20;
+    pixel_from_start = 200;
+}
+
+// Execute configureAccessibilitySettings function after accessibility_pro.js is loaded
+accessibilityScript.onload = configureAccessibilitySettings;
+
+// Append the script element to the document head
 document.head.appendChild(accessibilityScript);
-
-// Configuration for accessibility_pro.js
-var accessibility_rtl = true;
-var pixel_from_side = 300;
-var pixel_from_start = 300;
-
-// Your additional code for accessTool.js goes here
