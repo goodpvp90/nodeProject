f9a9fb9699f43e70f3cf66e284fecbea560e3b6a
goodpvp90 <59737598+goodpvp90@users.noreply.github.com>
2024-03-21 23:25:36 +0200

    little fix for signup

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -85,12 +85,16 @@ function validatePhoneNumber(phoneNumber) {
     if (phoneNumber.length != 10) {
         return "מספר הפלאפון צריך להכיל 10 ספרות";
     }
+    if (!/^0[0-9]{9}$/.test(phone)) {
+        return "מספר הפאלפון צריך להתחיל ב 0"
+    }
+
     return "נראה טוב!";
 }
 
 function validatePasswords(password, rpassword) {
     if (password != rpassword)
-        return "password do not match"
+        return "הסיסמאות לא תואמות"
     return "נראה טוב!";
 }
 
