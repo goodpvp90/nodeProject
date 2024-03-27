a39ee49eb827fc7d04132ef80dd5fbe5d5c6c034
Danny <vexdanny@gmail.com>
2024-03-27 03:35:43 +0200

    fixed selection and responsive bugs on sidebar

--- public/scripts/sideBarLayOut.js
+++ public/scripts/sideBarLayOut.js
@@ -1,8 +1,26 @@
 const hamBurger = document.querySelector(".toggle-btn");
+const sidebar = document.querySelector("#sidebar");
 
 hamBurger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
+  sidebar.classList.toggle("expand");
 });
 
+const sidebarItems = document.querySelectorAll(".sidebar-item");
 
+sidebarItems.forEach(function(item) {
+  item.addEventListener("click", function () {
+    if (!sidebar.classList.contains("expand")) {
+      sidebar.classList.add("expand");
+    }
+  });
+});
+
+const screenHeight = window.screen.height;
+if (window.matchMedia("(max-width: 500px) and (orientation: landscape)").matches) {
+    const screenHeight = window.screen.height;
+    document.getElementById("sidebar").classList.add("smallscreen");
+    document.querySelector(".smallscreen").style.height = screenHeight + "px";
+} else {
+    document.getElementById("sidebar").classList.remove("smallscreen");
+}
 
